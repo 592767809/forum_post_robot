b7ffde525ba3a69cea0822e34a18ce11471f0acc
592767809 <592767809@qq.com>
2023-02-22 00:14:47 +0800

    更新

--- bot.py
+++ bot.py
@@ -1,6 +1,34 @@
 
+import requests_html
 
-import telegram
+try:
+    import telegram
+except:
+    telegram = None
 
-bot = telegram.Bot('')
-bot.sendMessage()
+# bot = telegram.Bot('')
+# bot.sendMessage()
+
+
+class Bot(object):
+
+    TG = 1
+    DIS = 2
+    WX = 3
+
+    def __init__(self, bot_type):
+        assert bot_type in [1, 2, 3]
+        self.bot_type = bot_type
+
+    def init_bot(self, option):
+        pass
+
+    def send_message(self):
+        if self.bot_type == self.TG:
+            pass
+        elif self.bot_type == self.DIS:
+            pass
+        elif self.bot_type == self.WX:
+            pass
+        else:
+            raise Exception('')
